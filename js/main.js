0bda73fbc5576d3b0c9e9ed33673d7ff0137f432
K <kas.bihari@gmail.com>
2025-06-06 15:15:15 +0200

    portfolio v1 en mario erin gezet

--- js/main.js
+++ js/main.js
@@ -23,5 +23,5 @@ document.addEventListener("DOMContentLoaded", () => {
  window.addEventListener('load', function () {
     setTimeout(function () {
       document.body.classList.add('loaded');
-    }, 1000); // 1 seconden
+    }, 1); // 1 seconden
   });
